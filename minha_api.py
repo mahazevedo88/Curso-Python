39c21e5e9fc36b78bd5ea75c32f7c519066fa8f0
mahazevedo88 <azevedodearaujoma@gmail.com>
2025-02-26 16:28:09 -0300

    Update minha_api.py

--- minha_api.py
+++ minha_api.py
@@ -3,48 +3,46 @@ from requests import get
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def pagina_inicial():
-    return render_template('inicio.html')
+  return render_template('inicio.html')
+
 
 @app.route('/entrar/')
 def fazer_login():
-    return render_template('login.html')
+  return render_template('login.html')
+
 
 @app.route('/pagina403/')
 def pagina403():
-    return render_template('pagina403.html')
+  return render_template('pagina403.html')
 
-@app.route('/welcome')
+
+@app.route('/welcome/')
 def welcome():
-    return render_template('welcome.html')
+  return render_template('welcome.html')
 
-@app.route('/validador/', methods=['POST','GET'])
+
+@app.route('/validador/', methods=['POST', 'GET'])
 def validador():
-    acesso_u = 'mariana'
-    acesso_s = '456'
-    if request.method == 'POST': 
-        usuario = request.form['c_usuario']
-        senha = request.form['c_senha']
-        if usuario == acesso_u and senha == acesso_s:
-            return redirect(url_for('welcome'))
-        else: 
-            return redirect(url_for('pagina403'))
+  acesso_u = "caio"
+  acesso_s = "123"
+  if request.method == 'POST':
+    usuario = request.form['c_usuario']
+    senha = request.form['c_senha']
+    if usuario == acesso_u and senha == acesso_s:
+      return redirect(url_for('welcome'))
     else:
-        usuario = request.args.get('c_usuario')
-        senha = request.args.get('c_senha')
-        if usuario == acesso_u and senha == acesso_s:
-            return redirect(url_for('welcome'))
-        else: 
-            return redirect(url_for('pagina403'))
-    
+      return redirect(url_for('pagina403'))
+  else:
+    usuario = request.args.get('c_usuario')
+    senha = request.args.get('c_senha')
+    if usuario == acesso_u and senha == acesso_s:
+      return redirect(url_for('welcome'))
+    else:
+      return redirect(url_for('pagina403'))
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
-
-
-
-
-
-
-
+  app.run('0.0.0.0')
